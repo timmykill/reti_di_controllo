d9ce7a3e816daace5049c3951bde58dc2dc960c7
root <you@example.com>
2020-10-03 10:09:30 +0000

    primo commit

---
+++ 0/out_svolta/consumatore.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#define MAX_STRING_LENGTH 256
+
+//https://en.wikipedia.org/wiki/Tail_call#C_example
+void multiple_strstr(char * haystack, char* needle, int outfd){
+	char * start_strstr;
+	int haylen, needlen;
+	start_strstr = strstr(haystack, needle);
+	haylen = strlen(haystack);
+	needlen = strlen(needle);
+	if (start_strstr){
+		write(outfd, haystack, start_strstr - haystack);
+		multiple_strstr(start_strstr + needlen, needle, outfd);
+	} else {
+		write(outfd, haystack, haylen);
+	}
+}
+
+int main(int argc, char* argv[]){
+	char *file_in, read_char, buf[MAX_STRING_LENGTH], *needle, *start_strstr;
+	int nread, fd, i, outfd;
+	
+	if (argc != 3){ 
+		perror(" numero di argomenti sbagliato");
+		exit(1);
+	} 
+	
+	needle = argv[1];
+	
+	file_in = argv[2];
+	fd = open(file_in, O_RDONLY);
+	outfd = 1;
+	if (fd<0){
+		perror("P0: Impossibile aprire il file.");
+		exit(2);
+	}
+
+	for(i = 0; nread = read(fd, &read_char, sizeof(char)); i++){
+		if (nread >= 0){
+			buf[i] = read_char;
+			if (read_char == '\n'){
+				buf[i+1] = '\0';
+				multiple_strstr(buf, needle, outfd);
+				i=-1;
+			}
+		} else {
+			printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
+			perror("Errore!");
+			close(fd);
+			exit(3);
+		}
+	}
+
+	close(fd);
+}
---
+++ 0/out_svolta/produttore.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define MAX_STRING_LENGTH 256
+
+int main(int argc, char* argv[]){
+	int fd, readValues, bytes_to_write, written, i;
+	char *file_out, *ret;
+	char riga[MAX_STRING_LENGTH], buf[MAX_STRING_LENGTH];
+	
+	if (argc != 2){ 
+		perror(" numero di argomenti sbagliato"); exit(1);
+	} 
+	
+	file_out = argv[1];	
+	
+	fd = open(file_out, O_WRONLY|O_CREAT|O_TRUNC, 00640);
+	if (fd < 0){
+		perror("P0: Impossibile creare/aprire il file");
+		exit(2);
+	}
+	do {
+		printf("Inserisci la nuova riga\n");
+		ret = gets(riga); 
+		riga[strlen(riga)+1]='\0';  
+		riga[strlen(riga)]='\n';  
+		written = write(fd, riga, strlen(riga));
+		if (written < 0){
+			perror("P0: errore nella scrittura sul file");
+			exit(3);
+		}
+	} while (ret);	
+	close(fd);
+}
