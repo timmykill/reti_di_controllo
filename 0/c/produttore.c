61c17725594182b07d36346dfd768d2b2f2fd14a
Eimitfi <macarena7799@gmail.com>
2020-10-04 13:32:50 +0200

    patch a problema produttore che stampava l'ultima riga due volte nel file + piccole modifiche

--- 0/c/produttore.c
+++ 0/c/produttore.c
@@ -6,32 +6,30 @@
 #define MAX_STRING_LENGTH 256
 
 int main(int argc, char* argv[]){
-	int fd, readValues, bytes_to_write, written, i;
-	char *file_out, *ret;
-	char riga[MAX_STRING_LENGTH], buf[MAX_STRING_LENGTH];
+	int fd, written;
+	char *ret;
+	char riga[MAX_STRING_LENGTH];
 	
-	if (argc != 2){ 
+	if (argc != 2){
 		perror(" numero di argomenti sbagliato"); exit(1);
-	} 
+	}
 	
-	file_out = argv[1];	
-	
-	fd = open(file_out, O_WRONLY|O_CREAT|O_TRUNC, 00640);
+	fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 00640);
 	if (fd < 0){
 		perror("P0: Impossibile creare/aprire il file");
 		exit(2);
 	}
 	do {
 		printf("Inserisci la nuova riga\n");
-		ret = gets(riga); 
-		riga[strlen(riga)+1]='\0';  
+		ret = gets(riga);
 		if(ret){
-			riga[strlen(riga)]='\n';	
-		}
-		written = write(fd, riga, strlen(riga));
-		if (written < 0){
-			perror("P0: errore nella scrittura sul file");
-			exit(3);
+			riga[strlen(riga)+1]='\0';
+			riga[strlen(riga)]='\n';
+	                written = write(fd, riga, strlen(riga));
+	                if (written < 0){
+	                        perror("P0: errore nella scrittura sul file");
+	                        exit(3);
+	                }
 		}
 	} while (ret);	
 	close(fd);
