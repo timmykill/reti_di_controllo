d4fa3d1b17f6907fef0c1a753834fb4d60100223
Eimitfi <macarena7799@gmail.com>
2020-10-04 15:39:34 +0200

    Ripristino file_out per maggiore leggibilità

--- 0/c/produttore.c
+++ 0/c/produttore.c
@@ -7,14 +7,15 @@
 
 int main(int argc, char* argv[]){
 	int fd, written;
-	char *ret;
+	char *file_out,*ret;
 	char riga[MAX_STRING_LENGTH];
 	
 	if (argc != 2){
 		perror(" numero di argomenti sbagliato"); exit(1);
 	}
 	
-	fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 00640);
+	file_out=argv[1];
+	fd = open(file_out, O_WRONLY|O_CREAT|O_TRUNC, 00640);
 	if (fd < 0){
 		perror("P0: Impossibile creare/aprire il file");
 		exit(2);
