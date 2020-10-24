d2da179feecada2e7e047f1e6ac9d9f35199b44c
Eimitfi <macarena7799@gmail.com>
2020-10-24 17:52:54 +0200

    piccole correzioni varie, niente di troppo interessante

--- 3/materiale/clientD.c
+++ 3/materiale/clientD.c
@@ -13,7 +13,7 @@
 int main(int argc, char **argv){
 
 	struct hostent *host;
-	struct sockaddr_in clientaddr, servaddr;
+	struct sockaddr_in servaddr;
 	int  port, sd, num1, num2, len, ris;
 	char okstr[LINE_LENGTH];
 	char c;
@@ -24,41 +24,37 @@ int main(int argc, char **argv){
 		exit(1);
 	}
 
-	/* INIZIALIZZAZIONE INDIRIZZO CLIENT E SERVER --------------------- */
-	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in)); //mette 0 in clientaddr
-	clientaddr.sin_family = AF_INET;
-	clientaddr.sin_addr.s_addr = INADDR_ANY;
-	clientaddr.sin_port = 0;
+	        /* VERIFICA PORTA*/
+        num1 = 0;
+        while( argv[2][num1]!= '\0' ){
+                if( (argv[2][num1] < '0') || (argv[2][num1] > '9') ){
+                        printf("Secondo argomento non intero\n");
+                        printf("Error:%s serverAddress serverPort\n", argv[0]);
+                        exit(2);
+                }
+                num1++;
+        }
+        port = atoi(argv[2]); //porta server
+        if (port < 1024 || port > 65535){
+                printf("%s = porta scorretta...\n", argv[2]);
+                exit(2);
+        }
+
+	host = gethostbyname(argv[1]);
+        /* VERIFICA  HOST */
+        if (host == NULL){
+                printf("%s not found in /etc/hosts\n", argv[1]);
+                exit(2);
+        }
+
+
+	/* INIZIALIZZAZIONE INDIRIZZO SERVER --------------------- */
 
 	//indirizzo ip server
 	memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
 	servaddr.sin_family = AF_INET;
-	host = gethostbyname (argv[1]);
-
-	/* VERIFICA INTERO */
-	num1 = 0;
-	while( argv[2][num1]!= '\0' ){
-		if( (argv[2][num1] < '0') || (argv[2][num1] > '9') ){
-			printf("Secondo argomento non intero\n");
-			printf("Error:%s serverAddress serverPort\n", argv[0]);
-			exit(2);
-		}
-		num1++;
-	}
-	port = atoi(argv[2]); //porta server
-
-	/* VERIFICA PORT e HOST */
-	if (port < 1024 || port > 65535){
-		printf("%s = porta scorretta...\n", argv[2]);
-		exit(2);
-	}
-	if (host == NULL){
-		printf("%s not found in /etc/hosts\n", argv[1]);
-		exit(2);
-	}else{
-		servaddr.sin_addr.s_addr=((struct in_addr *)(host->h_addr))->s_addr;
-		servaddr.sin_port = htons(port);
-	}
+	servaddr.sin_addr.s_addr=((struct in_addr *)(host->h_addr))->s_addr;
+	servaddr.sin_port = htons(port);
 
 	/* CREAZIONE SOCKET ---------------------------------- */
 	sd = socket(AF_INET, SOCK_DGRAM, 0); //creazione socket
@@ -68,20 +64,13 @@ int main(int argc, char **argv){
 	}
 	printf("Client: creata la socket sd=%d\n", sd);
 
-	/* BIND SOCKET, a una porta scelta dal sistema --------------- */
-	if(bind(sd,(struct sockaddr *) &clientaddr, sizeof(clientaddr)) < 0){
-		perror("bind socket ");
-		exit(1);
-	}
-	printf("Client: bind socket ok, alla porta %i\n", clientaddr.sin_port);
-
 	/* CORPO DEL CLIENT: ciclo di accettazione di richieste da utente */
 	printf("Inserire nome file: \n");
 	while ((scanf("%s", okstr)) != EOF ){
 
 		/* richiesta operazione */
 		len = sizeof(servaddr);
-		if(sendto(sd, &okstr, LINE_LENGTH, 0, (struct sockaddr *)&servaddr, len) < 0){
+		if(sendto(sd, &okstr, strlen(okstr), 0, (struct sockaddr *)&servaddr, len) < 0){
 			perror("sendto");
 			continue;
 		}
--- 3/materiale/serverD.c
+++ 3/materiale/serverD.c
@@ -18,7 +18,7 @@ int searchWord(int fd);
 int main(int argc, char **argv){
 
 	int sd, port, len, num1, ris;
-  char req[N];
+	char req[N];
 	const int on = 1;
 	struct sockaddr_in cliaddr, servaddr;
 	struct hostent *clienthost;
@@ -28,23 +28,21 @@ int main(int argc, char **argv){
 		printf("Error: %s port\n", argv[0]);
 		exit(1);
 	}
-	else{
-		num1 = 0;
-		while(argv[1][num1] != '\0' ){
-			if((argv[1][num1] < '0') || (argv[1][num1] > '9')){
-				printf("Secondo argomento non intero\n");
-				printf("Error: %s port\n", argv[0]);
-				exit(1);
-			}
-			num1++;
+	num1 = 0;
+	while(argv[1][num1] != '\0' ){
+		if((argv[1][num1] < '0') || (argv[1][num1] > '9')){
+			printf("Secondo argomento non intero\n");
+			printf("Error: %s port\n", argv[0]);
+			exit(1);
 		}
-	  	port = atoi(argv[1]);
-  		if (port < 1024 || port > 65535){
-		      printf("Error: %s port\n", argv[0]);
-		      printf("1024 <= port <= 65535\n");
-		      exit(1);
-  		}
+		num1++;
 	}
+  	port = atoi(argv[1]);
+  	if (port < 1024 || port > 65535){
+	      printf("Error: %s port\n", argv[0]);
+	      printf("1024 <= port <= 65535\n");
+	      exit(1);
+  	}
 
 	/* INIZIALIZZAZIONE INDIRIZZO SERVER ---------------------------------- */
 	memset ((char *)&servaddr, 0, sizeof(servaddr)); //copia 0 in serveraddr per una lunghezza paria a servaddr
@@ -56,53 +54,53 @@ int main(int argc, char **argv){
 	sd = socket(AF_INET, SOCK_DGRAM, 0); //socket(dominio, tipo, protocollo) AF_INET famiglia internet,
                                        //SOCK_DGRAM socket datagram e protocollo 0 = protocollo di trasporto
 	if(sd < 0){
-    perror("creazione socket "); //errore creazione socket
-    exit(1);
-  }
+		perror("creazione socket "); //errore creazione socket
+		exit(1);
+	}
 	printf("Server: creata la socket, sd=%d\n", sd);
 
 	if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0){
-    perror("set opzioni socket ");
-    exit(1);
-  }
+		perror("set opzioni socket ");
+		exit(1);
+	}
 	printf("Server: set opzioni socket ok\n");
 
 	if(bind(sd,(struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
-    perror("bind socket ");
-    exit(1);
-  }
+		perror("bind socket ");
+		exit(1);
+	}
 	printf("Server: bind socket ok\n");
 
 	/* CICLO DI RICEZIONE RICHIESTE ------------------------------------------ */
 	for(;;){ //demone
 		len = sizeof(struct sockaddr_in);
 		if (recvfrom(sd, req, N, 0, (struct sockaddr *)&cliaddr, &len) < 0){
-      perror("recvfrom ");
-      continue;
-    }
+			perror("recvfrom ");
+			continue;
+		}
 
 		printf("Operazione richiesta su file: %s\n", req);
 		clienthost = gethostbyaddr( (char *) &cliaddr.sin_addr, sizeof(cliaddr.sin_addr), AF_INET);
 		if (clienthost == NULL){
-      printf("client host information not found\n");
-    }else{
-      printf("Operazione richiesta da: %s %i\n", clienthost->h_name,(unsigned)ntohs(cliaddr.sin_port));
-    }
+      			printf("client host information not found\n");
+    		}else{
+    			printf("Operazione richiesta da: %s %i\n", clienthost->h_name,(unsigned)ntohs(cliaddr.sin_port));
+    		}
 
 		/*EXEC*/
 		int fd = open(req, O_RDONLY, 0777);
-    if(fd < 0){
-      printf("Errore apertura file\n");
-      ris = -1;
-    }else{
-      ris = searchWord(fd);
-    }
-
+		if(fd < 0){
+			printf("Errore apertura file\n");
+ 		 	ris = -1;
+    		}else{
+     			ris = searchWord(fd);
+    		}
+		close(fd);
 		ris = htonl(ris);//conversione risposta
 		if (sendto(sd, &ris, sizeof(ris), 0, (struct sockaddr *)&cliaddr, len) < 0){
-      perror("sendto ");
-      continue;
-    }
+      			perror("sendto ");
+      			continue;
+    		}
 	} //for
 }
 
