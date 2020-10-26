fd66c07e9103b9877e6276d808665798ccea70a0
Eimitfi <macarena7799@gmail.com>
2020-10-26 04:34:09 +0100

    corretto mismatch nome file e file dato in input nello script

--- 3/materiale/test/run_clients.sh
+++ 3/materiale/test/run_clients.sh
@@ -4,7 +4,7 @@
 [ $# -ne 1 ] && echo "Usage: $0 numeroClienti" && exit 1
 
 for i in `seq 1 $1`; do {
-	echo -e "files/f_1m_line\n" | ./client localhost 50000 >> client.logs &
+	echo -e "files/f_1m_linee\n" | ./client localhost 50000 >> client.logs &
 	
 } done
 	
