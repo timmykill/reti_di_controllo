7e1b4598fab6f3cc5249e4bd171a3feb8b91ad83
Me <marko99thebest@gmail.com>
2020-11-04 17:53:15 +0100

    sono un coglione

--- 4/prova/server.c
+++ 4/prova/server.c
@@ -312,24 +312,48 @@ inline int replace_string_read(char* file, char* word)
 	char buf[READ_BUF_SIZE];
 	char * temp_file = "tempfile";
 	int count = 0;
+	unsigned int i, j, start_from_edge, alredy_checked_len, remaining_len;
+	char * edge_buf;
+	bool same = false;
 
 	orig_fd = open(file, O_RDONLY);
 	temp_fd = open(temp_file, O_WRONLY|O_CREAT|O_TRUNC, 0600);
 	orig_fd < 0 && die("lettura file, open", -100);
 	temp_fd < 0 && die("lettura file, open", -100);
-
+	
 	word_len = strlen(word);
-	while ((buf_len = read(orig_fd, buf, READ_BUF_SIZE)) > word_len) {
+	word_len >= READ_BUF_SIZE && die("buffer read troppo piccolo", -100);
+	while ((buf_len = read(orig_fd, buf, READ_BUF_SIZE)) > 0) {
 		multiple_strstr(buf, buf_len, word, word_len, temp_fd, &count);
-		/* to implement edge cases */
-		lseek(orig_fd, -word_len, SEEK_CUR);
-		lseek(temp_fd, -word_len, SEEK_CUR);
+		
+		/* this sould have a len of word_len - 1 */
+		edge_buf = buf + buf_len + 1 - word_len;
+		for (i = 0; i < word_len - 1 && same == false; i++){
+			if (edge_buf[i] == word[0]){
+				same = true;
+				start_from_edge = i;
+				for (j = 0; i < word_len - 1 && same == true; j++){
+					if (edge_buf[i + j] != word[j]){
+						same = false;
+					}
+				}
+			}
+		}
+		if (same == true){
+			remaining_len = start_from_edge + 1;
+			alredy_checked_len = word_len - remaining_len;
+			buf_len = read(orig_fd, buf, remaining_len);
+			if (buf_len == remaining_len && !memcmp(buf, word + alredy_checked_len, remaining_len)){
+				lseek(temp_fd, -alredy_checked_len, SEEK_CUR);	
+			} else {
+				/* can i write zero bytes? */
+				write(temp_fd, buf, buf_len);
+			}
+		}
 	}
-	if (buf_len > 0)
-		write(temp_fd, buf, buf_len);
-
+	
 	close(orig_fd);
 	close(temp_fd);
 	rename(temp_file, file);
-	return count;
+	return count; 
 }
